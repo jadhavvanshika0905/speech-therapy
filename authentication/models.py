7da5b537ffdcf78dec9efc312434d4373eb98791
Vanshika Jadhav <jadhavvanshika346@gmail.com>
2026-05-26 01:07:20 +0530

    Add files via upload

--- authentication/models.py
+++ authentication/models.py
@@ -30,6 +30,68 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Therapist(models.Model):
+    # 🔗 Connection with User
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='therapist_profile' )
+    # 👤 Basic Info
+    full_name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=15)
+    gender = models.CharField(max_length=10, blank=True)
+
+    # 🎓 Professional Info
+    specialization = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=200)
+    experience_years = models.IntegerField()
+
+    # 🕒 Work Info
+    availability = models.CharField(max_length=100, blank=True)  
+    # Example: "Mon-Fri 10AM-5PM"
+
+    supervisor = models.ForeignKey(
+        'Supervisor',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='therapists'
+    )
+
+
+    # ⭐ Performance
+    rating = models.FloatField(default=0)
+    total_reviews = models.IntegerField(default=0)
+
+    # 📅 Meta
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+    
+class Supervisor(models.Model):
+    # 🔗 Link with User
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='supervisor_profile'
+    )
+
+    # 👤 Basic Info
+    full_name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=15)
+
+    # 🏢 Professional Info
+    department = models.CharField(max_length=100)
+    experience_years = models.IntegerField()
+
+    # 📊 Work Info
+    total_therapists_managed = models.IntegerField(default=0)
+
+    # 📅 Meta
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
     
 
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-from .models import User
+from authentication.models import User
+from .models import Therapist,Supervisor
+from django.contrib.auth.decorators import login_required
+from therapy.models import Patient,Report,Session,TherapyPlan,Notification,SupervisorFeedback,SessionMessage
+
+
 
 # Create your views here.
 def login_view(request):
@@ -28,15 +33,51 @@ def login_view(request):
                 return redirect('patient_dashboard')
 
             else:
-                return redirect('home_page')  # fallback
+                return redirect('home_page')
 
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials'})
 
     return render(request, 'login.html')
 
+from django.contrib import messages
+
 def register_view(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        role = request.POST.get("role")
+        age = request.POST.get("age")
+        contact = request.POST.get("contact")
+        issue = request.POST.get("issue")
+
+        try:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password,
+                role=role.lower()
+            )
+
+            if role.lower() == "patient":
+                Patient.objects.create(
+                    user=user,
+                    name=username,
+                    age=age,
+                    contact=contact,
+                    speech_issue=issue
+                )
+
+            messages.success(request, "User created successfully")
+            return redirect('login_page')
+
+        except Exception as e:
+            print(e)
+            messages.error(request, "User creation failed")
+            return redirect('login_page')
+
+    return render(request, "register.html")
 
 def admin_dashboard(request):
     users = User.objects.all()
@@ -46,7 +87,6 @@ def admin_dashboard(request):
     patients_users = User.objects.filter(role='patient')
     patients = User.objects.filter(role='patient').select_related('therapist')
 
-    
     context = {
         'users': users,
         'therapists_list': therapists,
@@ -62,11 +102,103 @@ def admin_dashboard(request):
     
     return render(request, 'admin_dashboard.html', context)
 
-def supervisor_dashboard(request):
-    return render(request, 'supervisor_dashboard.html')
+from therapy.models import Patient, Session
+from django.db.models import Count
 
-def therapist_dashboard(request):
-    return render(request, 'therapist_dashboard.html')
+
+@login_required
+def supervisor_dashboard(request):
+    supervisor = request.user
+
+    if supervisor.role != 'supervisor':
+        return redirect('login')
+
+    therapist_users = User.objects.filter(role='therapist', supervisor=supervisor)
+    patients = Patient.objects.filter(
+        assigned_therapist__in=therapist_users
+    ).select_related('assigned_therapist')
+
+    therapist_profiles = Therapist.objects.filter(
+        user__in=therapist_users
+    ).select_related('user').annotate(
+        total_patients=Count('user__assigned_patients')
+    )
+
+    pending_plans = TherapyPlan.objects.filter(
+        therapist__in=therapist_users,
+        status='pending'
+    )
+    reports = Report.objects.filter(
+        plan__therapist__in=therapist_users
+    ).select_related('plan', 'plan__patient', 'plan__therapist')
+
+    unread_notifications = Notification.objects.filter(
+        user=supervisor,
+        is_read=False
+    )
+
+    context = {
+        "therapist_count":     therapist_users.count(),
+        "patient_count":       patients.count(),
+        "completed":           patients.filter(status='completed').count(),
+        "ongoing":             patients.filter(status='ongoing').count(),
+        "therapist_profiles":  therapist_profiles,
+        "patients":            patients,
+        "pending_plans_count": pending_plans.count(),
+        "unread_count":        unread_notifications.count(),
+        "notifications":       unread_notifications[:5],
+        "reports":             reports,
+    }
+
+    return render(request, "supervisor_dashboard.html", context)
+
+
+@login_required
+def therapist_dashboard(request):    
+    patients = Patient.objects.filter(assigned_therapist=request.user)
+    plans = TherapyPlan.objects.filter(therapist=request.user).select_related('patient')
+    total     = patients.count()
+    completed = patients.filter(status='completed').count()
+    ongoing   = patients.filter(status='ongoing').count()
+
+    # Har patient ke liye active_session_id aur unread_count
+    for patient in patients:
+        # ✅ FIX: Session.plan.patient ke through filter karo
+        session = Session.objects.filter(plan__patient=patient).order_by('-id').first()
+        patient.active_session_id = session.id if session else None
+
+        if session:
+            patient.unread_count = SessionMessage.objects.filter(
+                session=session,
+                is_read=False,
+            ).exclude(sender=request.user).count()
+        else:
+            patient.unread_count = 0
+
+    feedbacks = SupervisorFeedback.objects.filter(
+        plan__therapist=request.user
+    ).select_related('plan', 'plan__patient').order_by('-id')
+    
+    reports = Report.objects.filter(
+        plan__therapist=request.user
+    ).select_related('plan', 'plan__patient')
+
+    unread_notifications = Notification.objects.filter(user=request.user, is_read=False)
+    
+    context = {
+        'patients':      patients,
+        'total':         total,
+        'plans':         plans,
+        'completed':     completed,
+        'ongoing':       ongoing,
+        'pending_plans': plans.filter(status='pending').count(),
+        'unread_count':  unread_notifications.count(),
+        'notifications': unread_notifications[:5],
+        'feedbacks':     feedbacks,
+        'reports':       reports,
+    }
+    return render(request, 'therapist_dashboard.html', context)
+
 
 def add_user_view(request):
     if request.method == "POST":
@@ -74,10 +206,44 @@ def add_user_view(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         role = request.POST.get("role")
-        user = User.objects.create_user(username = username,email=email,password=password,role=role)
-        user.save()
-        return redirect('admin_dashboard')
-    return render(request,"add_user.html")
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            role=role
+        )
+
+        if role == "therapist":
+            Therapist.objects.create(
+                user=user,
+                full_name=username,
+                specialization=request.POST.get("specialization"),
+                qualification=request.POST.get("qualification"),
+                experience_years=request.POST.get("experience") or 0
+            )
+
+        elif role == "patient":
+            Patient.objects.create(
+                user=user,
+                name=username,
+                age=request.POST.get("age"),
+                contact=request.POST.get("contact"),
+                speech_issue=request.POST.get("issue")
+            )
+
+        elif role == "supervisor":
+            Supervisor.objects.create(
+                user=user,
+                full_name=username,
+                contact=request.POST.get("contact"),
+                experience_years=request.POST.get("experience_supervisor") or 0,
+                department=request.POST.get("department"),
+            )
+
+        return redirect('login_page')
+    return render(request, "add_user.html")
+
     
 def edit_user_view(request, id):
     user = User.objects.get(id=id)
@@ -96,20 +262,20 @@ def edit_user_view(request, id):
 
     return render(request, 'edit_user.html', {'user': user})
 
-def delete_user_view(request,id):
+def delete_user_view(request, id):
     user = User.objects.get(id=id)
     user.delete()
     return redirect("admin_dashboard")
 
-def disable_user_view(request,id):
+def disable_user_view(request, id):
     user = User.objects.get(id=id)
-    user.is_active=False
+    user.is_active = False
     user.save()
     return redirect("admin_dashboard")
 
-def enable_user_view(request,id):
+def enable_user_view(request, id):
     user = User.objects.get(id=id)
-    user.is_active=True
+    user.is_active = True
     user.save()
     return redirect("admin_dashboard")
 
@@ -121,11 +287,24 @@ def assign_therapist(request):
     if request.method == 'POST':
         patient_id = request.POST.get('patient_id')
         therapist_id = request.POST.get('therapist_id')
-        patient = User.objects.get(id=patient_id, role='patient')
+        
+        patient_user = User.objects.get(id=patient_id, role='patient')
         therapist = User.objects.get(id=therapist_id, role='therapist')
 
-        patient.therapist = therapist
-        patient.save()
+        patient_user.therapist = therapist
+        patient_user.save()
+
+        try:
+            patient_profile = Patient.objects.get(user=patient_user)
+            patient_profile.assigned_therapist = therapist
+            patient_profile.save()
+        except Patient.DoesNotExist:
+            Patient.objects.create(
+                user=patient_user,
+                name=patient_user.username,
+                contact=patient_user.email,
+                assigned_therapist=therapist
+            )
 
         return redirect('admin_dashboard')
 
@@ -157,6 +336,3 @@ def assign_supervisor(request):
         'therapists': therapists,
         'supervisors': supervisors
     })
-
-
-
